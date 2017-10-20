0f4dd026d36e6ff08613497af680d79ce1664e3d
MrFix0r <mrfix0r@gmail.com>
2017-10-20 16:22:23 +0300

    валидация xml и xsd

--- src/main/java/com/bivgroup/Main.java
+++ src/main/java/com/bivgroup/Main.java
@@ -1,14 +1,19 @@
 package com.bivgroup;
 
+import xmlSecondStructure.Cathedra;
 import xmlSecondStructure.Faculty;
 import xmlSecondStructure.University;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.SchemaFactory;
+import java.io.File;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         University uni = new University();
 
         Faculty faculty1 = new Faculty();
@@ -19,20 +24,41 @@ public class Main {
         uni.getFaculty().add(faculty1);
         uni.getFaculty().add(faculty2);
 
-
         try {
 
+            File file = new File("./target/test.xml");
             JAXBContext jaxbContext = JAXBContext.newInstance(University.class);
             Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 
             // output pretty printed
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
+            jaxbMarshaller.marshal(uni, file);
             jaxbMarshaller.marshal(uni, System.out);
 
         }
         catch (JAXBException e) {
             e.printStackTrace();
         }
+
+        //валидация
+
+        validateXMLByXSD(new File("./src/main/resources/xml/uni.xml"), new File("./src/main/resources/xml/uni3.xsd"));
+        validateXMLByXSD(new File("./target/test2.xml"), new File("./src/main/resources/xml/uni3.xsd") );
+
+    }
+
+    public static void validateXMLByXSD(File xml, File xsd) {
+        try {
+            SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
+                    .newSchema(xsd)
+                    .newValidator()
+                    .validate(new StreamSource(xml));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Файл \'" + xml.getName() + "\' не валиден с файлом \'" + xsd.getName() + "\'");
+            return;
+        }
+        System.out.println("Файл \'" + xml.getName() + "\' валиден с файлом \'" + xsd.getName() + "\'");;
     }
 }
